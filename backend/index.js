6033282959d0293c5a1c189a2378d793ec3f9528
PrathamMalvia <malviapratham1@gmail.com>
2024-06-10 14:50:53 +0530

    add mongodb connection

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 // Libraries
 const express = require("express");
 require('dotenv').config();
+const mongoose = require("mongoose");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -9,6 +10,14 @@ app.get("/", (req, res) => {
     res.send("Hello");
 });
 
+mongoose.connect(process.env.MONGODB_URL)
+    .then(() => {
+        console.log("App connected to database");
+    })
+    .catch((error) => {
+        console.log(error);
+    })
+    
 app.listen(PORT, (req, res) => {
     console.log(`Server is running on PORT ${process.env.PORT}`);
 })
