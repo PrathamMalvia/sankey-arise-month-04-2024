1b9ee635d6ef55f96364fce2fa4e8834e11051bc
PrathamMalvia <malviapratham1@gmail.com>
2024-06-11 01:50:49 +0530

    add dummy homepage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,14 @@
 import { Routes, Route } from "react-router-dom"
 import RegisterPage from "./pages/RegisterPage"
 import LoginPage from "./pages/LoginPage"
+import HomePage from "./pages/HomePage"
 
 const App = () => {
   return (
     <Routes>
-      {/* <Route path="/" element={<LoginPage />} /> */}
       <Route path="/register" element={<RegisterPage />} />
       <Route path="/login" element={<LoginPage />} />
+      <Route path="/" element={<HomePage />} />
     </Routes>
   )
 }
